7dda949a16603f8faf786d2f166075a0a242f33e
Eric Kennedy <ekeric13@gmail.com>
2015-02-11 16:48:13 -0800

    Fix syntax to fit production variable norm

--- server/config/productionConfig.js
+++ server/config/productionConfig.js
@@ -1,6 +1,6 @@
 var config = {
-  clientIDpro: process.env.clientID,
-  clientSecretpro: process.env.clientSecret,
+  clientIDpro: process.env.CLIENT_ID,
+  clientSecretpro: process.env.CLIENT_SECRET,
   callbackURLpro: "https://brainstormideation.herokuapp.com/auth/callback",
   MONGOLAB_URI: "mongodb://heroku_app33914307:uoptf0qmv9gma56c98gt98qks9@ds041871.mongolab.com:41871/heroku_app33914307"
 
