c8b0a8136eee2cf91e5b5cdf6afca41956e66764
Eric Kennedy <ekeric13@gmail.com>
2015-02-11 16:44:26 -0800

    Add github client info for production

---
+++ server/config/productionConfig.js
@@ -0,0 +1,9 @@
+var config = {
+  clientIDpro: process.env.clientID,
+  clientSecretpro: process.env.clientSecret,
+  callbackURLpro: "https://brainstormideation.herokuapp.com/auth/callback",
+  MONGOLAB_URI: "mongodb://heroku_app33914307:uoptf0qmv9gma56c98gt98qks9@ds041871.mongolab.com:41871/heroku_app33914307"
+
+};
+module.exports = config;
+
